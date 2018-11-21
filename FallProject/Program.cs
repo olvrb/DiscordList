20477a643ab3267583c907faf4f0953a8998e8ce
Oliver Boudet <oboudet1@me.com>
2018-11-21 13:39:31 +0100

    trim

--- FallProject/Program.cs
+++ FallProject/Program.cs
@@ -23,7 +23,7 @@ namespace FallProject {
                                                                 .GetResult();
 
         public async Task RunBotAsync() {
-            Token     = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "token.txt"));
+            Token     = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "token.txt")).Trim();
             Console.WriteLine(Token);
             _client   = new DiscordSocketClient();
             _commands = new CommandService();
